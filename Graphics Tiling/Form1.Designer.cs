a3bcc6f76a250c39b3598b6c8e40116598c06527
lprincip4340 <lprincip4340@C5082UA8281ZT4>
2024-05-08 08:45:43 -0400

    Add project files.

---
+++ Graphics Tiling/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Graphics_Tiling
+{
+    partial class frmTiling1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTiling1));
+            this.btnDrawTiles = new System.Windows.Forms.Button();
+            this.Walter = new System.Windows.Forms.PictureBox();
+            this.Gras = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Walter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDrawTiles
+            // 
+            this.btnDrawTiles.Location = new System.Drawing.Point(12, 399);
+            this.btnDrawTiles.Name = "btnDrawTiles";
+            this.btnDrawTiles.Size = new System.Drawing.Size(117, 39);
+            this.btnDrawTiles.TabIndex = 0;
+            this.btnDrawTiles.Text = "Draw Tiles";
+            this.btnDrawTiles.UseVisualStyleBackColor = true;
+            this.btnDrawTiles.Click += new System.EventHandler(this.btnDrawTiles_Click);
+            // 
+            // Walter
+            // 
+            this.Walter.Image = ((System.Drawing.Image)(resources.GetObject("Walter.Image")));
+            this.Walter.Location = new System.Drawing.Point(50, 361);
+            this.Walter.Name = "Walter";
+            this.Walter.Size = new System.Drawing.Size(32, 32);
+            this.Walter.TabIndex = 1;
+            this.Walter.TabStop = false;
+            // 
+            // Gras
+            // 
+            this.Gras.Image = ((System.Drawing.Image)(resources.GetObject("Gras.Image")));
+            this.Gras.Location = new System.Drawing.Point(12, 361);
+            this.Gras.Name = "Gras";
+            this.Gras.Size = new System.Drawing.Size(32, 32);
+            this.Gras.TabIndex = 2;
+            this.Gras.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(135, 399);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(116, 39);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(257, 399);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(116, 39);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // frmTiling1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 444);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.Gras);
+            this.Controls.Add(this.Walter);
+            this.Controls.Add(this.btnDrawTiles);
+            this.Name = "frmTiling1";
+            this.Text = "TILES!";
+            this.Load += new System.EventHandler(this.frmTiling1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmTiling1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.Walter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gras)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDrawTiles;
+        private System.Windows.Forms.PictureBox Walter;
+        private System.Windows.Forms.PictureBox Gras;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Graphics Tiling/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Graphics_Tiling
+{
+    public partial class frmTiling1 : Form
+    {
+        public frmTiling1()
+        {
+            InitializeComponent();
+        }
+        Graphics G;
+        int tileSize = 32;
+        int newX;
+        int newY;
+
+        int[,] mCord = new int[12,12];
+        int xCord;
+        int yCord;
+
+        Bitmap bmpGrass;
+        Bitmap bmpWater;
+
+        // Draws a bunch of black tiles in every single square.
+        private void btnDrawTiles_Click(object sender, EventArgs e)
+        {
+            G = this.CreateGraphics();
+            for(int x = 0; x < this.Width; x += tileSize)
+            {
+                for (int y = 0; y < this.Height; y += tileSize)
+                {
+                    G.FillRectangle(Brushes.Black, x, y, tileSize, tileSize);
+                    G.DrawRectangle(Pens.PaleVioletRed, x, y, tileSize, tileSize);;
+                }
+            }
+            G.Dispose();
+        }
+
+        private void frmTiling1_Load(object sender, EventArgs e)
+        {
+            bmpGrass = new Bitmap(Gras.Image);
+            bmpWater = new Bitmap(Walter.Image);
+            Gras.Hide();
+            Walter.Hide();
+        }
+
+        private void frmTiling1_MouseMove(object sender, MouseEventArgs e)
+        {
+            G = this.CreateGraphics();
+            newX = e.X - e.X % tileSize;
+            newY = e.Y - e.Y % tileSize;
+
+            xCord = newX / tileSize;
+            yCord = newY / tileSize;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                G.DrawImage(bmpGrass, newX, newY, tileSize, tileSize);
+                mCord[xCord, yCord] = 1;
+
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                G.DrawImage(bmpWater, newX, newY, tileSize, tileSize);
+                mCord[xCord, yCord] = 2;
+            }
+           
+            G.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string filepath = Application.StartupPath + "\\Map.txt";
+            FileStream fs = new FileStream(filepath, FileMode.Create);
+            StreamWriter myFile = new StreamWriter(fs);
+
+            for (int x = 0; x < mCord.GetLength(0); x++)
+            {
+                for (int y = 0; y < mCord.GetLength(1); y++)
+                {
+                    myFile.WriteLine(mCord[x, y]);
+                }
+            }
+            myFile.Close();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string filepath = Application.StartupPath + "\\Map.txt";
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            StreamReader myFile = new StreamReader(fs);
+
+            for (int x = 0; x < mCord.GetLength(0); x++)
+            {
+                for (int y = 0; y < mCord.GetLength(1); y++)
+                {
+                    G = this.CreateGraphics();
+                    int line = int.Parse(myFile.ReadLine());
+
+                    if (line == 1)
+                    {
+                        G.DrawImage(bmpGrass, x * tileSize, y * tileSize);
+                    }
+
+                    else if (line == 2)
+                    {
+                        G.DrawImage(bmpWater, x * tileSize, y * tileSize);
+                    }
+                    else
+                    {
+                        G.FillRectangle(Brushes.Black, x*tileSize, y*tileSize, tileSize, tileSize);
+                        G.DrawRectangle(Pens.PaleVioletRed, x*tileSize, y*tileSize, tileSize, tileSize);
+                    }
+                }
+            }
+            myFile.Close();
+        }
+    }
+}
